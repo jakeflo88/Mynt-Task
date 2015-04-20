90138c38111bab63a4126661952e56629c85553c
jakeflo88 <jakeflorian.mcf@gmail.com>
2015-04-20 12:42:58 -0300

    Implement Offer service in Browse controller

--- scripts/services/offer.js
+++ scripts/services/offer.js
@@ -1,8 +1,9 @@
 'use strict'
 
-app.factory('Offer', function(FURL, $firebase) {
+app.factory('Offer', function(FURL, $firebase, $q, Auth) {
 
 	var ref = new Firebase(FURL);
+	var user = Auth.user;
 
 	var Offer = {
 
@@ -16,7 +17,25 @@ app.factory('Offer', function(FURL, $firebase) {
 			if(task_offers) {
 				return task_offers.$add(offer);
 			}
+		},
+
+		isOffered: function(taskId) {
+			if(user && user.provider) {
+				var d = $q.defer();
+
+				$firebase(ref.child('offers').child(taskId).orderByChild('uid')
+					.equalTo(user.uid))
+					.$asArray()
+					.$loaded().then(function(data) {
+						d.resolve(data.length > 0);
+					}, function () {
+						d.reject(false);
+					});
+
+				return d.promise;
+			}
 		}
+
 
 	};
 
